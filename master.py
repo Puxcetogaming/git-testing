7cba0d3041c2280b60e1feeaee1554be6c6203f0
Iliyan <puxcetogaming@gmail.com>
2024-07-03 22:20:22 +0300

    Planted bush

--- master.py
+++ master.py
@@ -3,12 +3,14 @@ import time
 i = 0
 
 while True:
+    print("Moved north")
     if i != 5:
         print("Can't Harvest")
         i = i + 1
         time.sleep(1)
     else:
         print("Can Harvest")
+        print("Planted bush")
         i = 1
         time.sleep(1)
     
